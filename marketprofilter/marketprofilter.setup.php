64f6ff7345f96d19c476bbb31fa6dc3645e381aa
sodium carbonate <webitproff@gmail.com>
2026-05-12 00:09:57 +0300

    CMF Cotonti v.1+ May 11Th, 2026 version 3.3.36
    Updated version number and added default language configuration.

--- marketprofilter/marketprofilter.setup.php
+++ marketprofilter/marketprofilter.setup.php
@@ -4,10 +4,11 @@
 Code=marketprofilter
 Name=Market PRO Filter
 Description=Dynamic filter parameters for Market PRO module
-Version=2.2.1
-Date=2025-12-14
+Version=3.3.36
+Date=May 11Th, 2026
 Author=webitproff
 Copyright=(c) webitproff <a href="https://github.com/webitproff?tab=repositories" target="_blank" rel="noopener noreferrer"><strong>See on GitHub</strong></a>
+Notes=
 Auth_guests=R
 Lock_guests=W12345
 Auth_members=R
@@ -18,20 +19,28 @@ Recommends_modules=
 Recommends_plugins=
 [END_COT_EXT]
 [BEGIN_COT_EXT_CONFIG]
-marketprofilter_log_enable=01:radio::0:Использовать функцию логирования 
+marketprofilter_defaultlang=01:select:ru,ua,en:en:Код языка по умолчанию для фильтра
+marketprofilter_log_enable=11:radio::0:Использовать функцию логирования 
 [END_COT_EXT_CONFIG]
 ==================== */
 
 defined('COT_CODE') or die('Wrong URL');
 
 /**
- * Market PRO Filter plugin for CMF Cotonti Siena v.0.9.26, PHP v.8.4+, MySQL v.8.0+
- * Filename: marketprofilter.admin.php
+ * Market PRO Filter plugin for CMF Cotonti v.1+, PHP v.8.4+, MySQL v.8.0+
+ * Filename: marketprofilter.setup.php
  * Purpose: Registers metadata of the marketprofilter plugin in the DB Cotonti.
- * Date=2025-12-14
+ * Date=May 11Th, 2026
+ *
+ * ReadMeMore:              https://abuyfile.com/market/cotonti/plugs/market-pro-filter 
+ * Support:                 https://abuyfile.com/forums/cotonti/custom/plugs/marketprofilter
+ *
+ * Plugin Market PRO Filter (Source code):  https://github.com/webitproff/marketprofilter-cotonti
+ * Module Market PRO (Source code):         https://github.com/webitproff/marketpro-cotonti
+ *
  * @package marketprofilter
- * @version 2.2.1
+ * @version 3.3.36
  * @author webitproff
- * @copyright Copyright (c) webitproff 2025 https://github.com/webitproff/
+ * @copyright Copyright (c) webitproff 2026 https://github.com/webitproff/
  * @license BSD
  */
