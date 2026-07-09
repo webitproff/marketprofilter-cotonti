9668698a8c144c4665fe8c0c927ea0a330ec9efb
webitproff <webitproff@gmail.com>
2026-07-09 20:46:09 +0300

    Delete marketprofilter directory

--- marketprofilter/setup/marketprofilter.uninstall.sql
+++
@@ -1,8 +0,0 @@
--- marketprofilter.uninstall.sql
-
--- marketprofilter.uninstall.sql
--- Полное удаление плагина Market PRO Filter (включая мультиязычность)
-
-DROP TABLE IF EXISTS `cot_marketprofilter_params_values`;
-DROP TABLE IF EXISTS `cot_marketprofilter_i18n`;
-DROP TABLE IF EXISTS `cot_marketprofilter_params`;
